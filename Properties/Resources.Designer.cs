efbbe254a6667d138919f0db75dc0d29ccd2ddaa
leegunwoo123 <lkw4165@naver.com>
2025-03-05 13:06:26 +0900

    vololand logo

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -1759,6 +1759,16 @@ namespace MissionPlanner.Properties {
         /// <summary>
         ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
+        internal static System.Drawing.Bitmap vololand_logo {
+            get {
+                object obj = ResourceManager.GetObject("vololand logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
         internal static System.Drawing.Bitmap wizardicon {
             get {
                 object obj = ResourceManager.GetObject("wizardicon", resourceCulture);

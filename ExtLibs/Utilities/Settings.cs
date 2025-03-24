06ba170c1acf4df2259d58110e39d9e829470e4a
leegunwoo123 <lkw4165@naver.com>
2025-03-24 14:43:01 +0900

    config.xml 저장 경로 변경

--- ExtLibs/Utilities/Settings.cs
+++ ExtLibs/Utilities/Settings.cs
@@ -17,7 +17,7 @@ namespace MissionPlanner.Utilities
     {
         static Settings _instance;
 
-        public static string AppConfigName { get; set; } = "Mission Planner";
+        public static string AppConfigName { get; set; } = "VOLOLAND";
 
         public static Settings Instance
         {
